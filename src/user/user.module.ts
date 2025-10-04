7a9a05c4a5bc58c63904128b9e1eb89f240ca194
codedcoder <codedcoderrr@gmail.com>
2025-10-04 17:41:47 +0100

    auth

---
+++ src/user/user.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { UserSchema } from './schema/user.schema';
+import { SchemaNames } from '../constants/schema-constants';
+import { Logger } from '@nestjs/common';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: SchemaNames.USER, schema: UserSchema }]),
+  ],
+  providers: [UserService, Logger],
+  controllers: [UserController],
+  exports: [UserService, MongooseModule],
+})
+export class UserModule {}

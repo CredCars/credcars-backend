6a655acca1ab7fae1124c411bf106be4a9e2e474
codedcoder <codedcoderrr@gmail.com>
2025-10-05 09:32:04 +0100

    docker fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import configuration from '@config/configuration';
+import corsConfig from './cors.config';
+import * as session from 'express-session';
+import * as path from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

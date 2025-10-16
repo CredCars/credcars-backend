281d9cea4eeb2b2a416a0d8d02942be47aa993e9
codedcoder <codedcoderrr@gmail.com>
2025-10-16 19:18:38 +0100

    fix

--- infra/terraform/production/main.tf
+++ infra/terraform/production/main.tf
@@ -15,5 +15,11 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
+}
+
+resource "aws_elastic_beanstalk_environment" "production" {
+  name        = "Credcars-backend-production-env"
+  application = aws_elastic_beanstalk_application.app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v6.6.6 running Node.js 20"
 }

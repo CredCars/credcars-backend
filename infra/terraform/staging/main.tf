b2d5a4295b1ba01fa294074ca70a0847ebb437ba
codedcoder <codedcoderrr@gmail.com>
2025-10-16 21:34:50 +0100

    fix

--- infra/terraform/staging/main.tf
+++ infra/terraform/staging/main.tf
@@ -15,12 +15,11 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
 resource "aws_elastic_beanstalk_environment" "staging" {
   name        = "${var.app_name}-staging-env"
   application = aws_elastic_beanstalk_application.app.name
   solution_stack_name = "64bit Amazon Linux 2023 v6.6.6 running Node.js 20"
-  # ... staging-specific settings like instance type, env vars
 }

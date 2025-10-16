89832998865a32abe87cc6eb9e5781845fb8ca0c
codedcoder <codedcoderrr@gmail.com>
2025-10-16 10:24:05 +0100

    fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -8,6 +8,14 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket = "credcars-terraform-state"
+    key    = "backend/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }

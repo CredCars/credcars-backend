7ea19e2c1eaba3bf730a8d808deb635380573664
codedcoder <codedcoderrr@gmail.com>
2025-10-16 10:07:21 +0100

    fix

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,8 @@
+output "beanstalk_environment_url" {
+  description = "Elastic Beanstalk environment URL"
+  value       = aws_elastic_beanstalk_environment.env.endpoint_url
+}
+output "beanstalk_app_bucket" {
+  description = "Elastic Beanstalk S3 bucket for application versions"
+  value       = aws_s3_bucket.beanstalk_app_bucket.id
+}
--- terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "beanstalk_environment_url" {
-  description = "Elastic Beanstalk environment URL"
-  value       = aws_elastic_beanstalk_environment.env.endpoint_url
-}

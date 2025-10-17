2361108d4bb68c0bc426c82da193e595f935e31c
codedcoder <codedcoderrr@gmail.com>
2025-10-17 20:56:48 +0100

    fix

---
+++ infra/terraform/staging/variables.tf
@@ -0,0 +1,45 @@
+variable "env" {
+  description = "Deployment environment name (e.g., staging, production)"
+  type = string
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default = "us-east-1"
+}
+
+variable "port" {
+  description = "Elastic Beanstalk application name"
+  type        = string
+  default = "8080"
+}
+
+variable "app_name" {
+  description = "Elastic Beanstalk application name"
+  type        = string
+  default = "Credcars-backend"
+}
+
+variable "instance_type" {
+  description = "Elastic Beanstalk application name"
+  type        = string
+  default = "t3.micro"
+}
+
+variable "load_balancer_type" {
+  description = "Elastic Beanstalk application name"
+  type        = string
+  default = "classic"
+}
+
+variable "app_zip_path" {
+  description = "Path to the Beanstalk application zip file"
+  type        = string
+  default     = "app.zip"
+}
+
+variable "database_url" {
+  description = "Your database's url"
+  type        = string
+}
